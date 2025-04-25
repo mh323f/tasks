8496378f1bc7cba3dd5828bd5e2976cbad598218
mh323f <105400312+mh323f@users.noreply.github.com>
2025-04-25 17:37:12 -0600

    Configuración de BD
    Se carga el archivo de conexión a la bases de datos

---
+++ config/db.php
@@ -0,0 +1,16 @@
+﻿<?php
+
+class db{
+       
+     public static function conexion(){
+        $mysqli=new mysqli('localhost', 'root', 'Ma13he052010', 'tasks');
+        $mysqli->query("SET NAMES 'utf8'");
+
+      if ($mysqli->connect_errno) {
+        echo "Conexion fallida a Bd MySQL:" . $mysqli->connect_error;
+        exit();
+      }
+        return $mysqli;
+    }
+}
+?>
